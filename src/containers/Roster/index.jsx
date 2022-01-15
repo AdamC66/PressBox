464a12756f61bc9c163cfeabc71287a36cdd91a6
AdamC66 <adam.cote66@gmail.com>
2022-01-15 16:28:55 -0500

    really coming along

--- src/containers/Roster/index.jsx
+++ src/containers/Roster/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import * as _ from "lodash";
+import clsx from "clsx";
 import { useParams } from "react-router-dom";
 import { backendFetch } from "utils/api";
 import { useQuery } from "react-query";
@@ -7,11 +9,23 @@ import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 import PlayerCard from "./PlayerCard";
 import classes from "./style.module.css";
+
+const getForwardPosition = (num) => {
+  const positions = ["LW", "C", "RW"];
+  return positions[num];
+};
+
 function Roster() {
   const { teamCode } = useParams();
-  const [forwards, setForwards] = React.useState([]);
+  const [forwards, setForwards] = React.useState({
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+  });
   const [defensemen, setDefensemen] = React.useState([]);
   const [goalies, setGoalies] = React.useState([]);
+  const [players, setPlayers] = React.useState([]);
   const [bench, setBench] = React.useState([]);
   const getLineupGrid = (lineSize, playerGroup) => {
     const playerGroupArrays = [];
@@ -19,6 +33,9 @@ function Roster() {
       playerGroupArrays.push(playerGroup.slice(i, i + lineSize));
     }
     return playerGroupArrays;
+  };
+  const getPlayerByCode = (code) => {
+    return _.find(players, { id: code });
   };
   const { data: rosterData, isLoading, isError } = useQuery(
     ["roster", teamCode],
@@ -29,35 +46,13 @@ function Roster() {
       }),
     {
       onSuccess: (data) => {
-        const tempBench = [];
-        // Set the first 12 players to forwards and the rest to bench
-        const forwardData = data.filter(
-          (player) =>
-            player.positionAbbv === "LW" ||
-            player.positionAbbv === "RW" ||
-            player.positionAbbv === "C"
-        );
-        setForwards(getLineupGrid(3, forwardData.slice(0, 12)));
-        tempBench.push(...forwardData.slice(12));
-
-        // Set the first 6 defencemen to defensemen and the rest to bench
-        const defensemenData = data.filter(
-          (player) => player.positionAbbv === "D"
-        );
-        setDefensemen(defensemenData.slice(0, 6));
-        tempBench.push(...defensemenData.slice(6));
-
-        // Set the first 2 goalies to goalies and the rest to bench
-        const goalieData = data.filter((player) => player.positionAbbv === "G");
-        setGoalies(goalieData.slice(0, 2));
-        tempBench.push(...goalieData.slice(2));
-
-        setBench(tempBench);
+        setPlayers(data);
       },
     }
   );
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error!</p>;
+
   const onDragEnd = (result) => {
     console.log(result);
     const { source, destination } = result;
@@ -66,30 +61,101 @@ function Roster() {
       droppableId: destinationArea,
       index: destinationIndex,
     } = destination;
-    const sourceLineIndex = parseInt(sourceArea.split("-")[1]);
-    const destinationLineIndex = parseInt(destinationArea.split("-")[1]);
-    console.log(sourceArea, destinationArea);
-    console.log(sourceIndex, destinationIndex);
-    const newLineup = [...forwards];
-    const sourcePlayer = newLineup[sourceLineIndex][sourceIndex];
-    const destinationPlayer = newLineup[destinationLineIndex][destinationIndex];
-    console.log(sourcePlayer);
-    console.log(destinationPlayer);
-    if (destinationPlayer === undefined) {
-      newLineup[destinationLineIndex][destinationIndex] = sourcePlayer;
-      newLineup[sourceLineIndex][sourceIndex] =
-        newLineup[destinationLineIndex][
-          newLineup[destinationLineIndex].length - 1
-        ];
-    } else {
-      newLineup[sourceLineIndex][sourceIndex] = destinationPlayer;
+    const sourceLineIndex = sourceArea.split("-");
+    const destinationLineIndex = destinationArea.split("-");
+    console.log(sourceLineIndex, sourceIndex);
+    console.log(destinationLineIndex, destinationIndex);
+    const sourceSection = sourceLineIndex[0];
+    const destinationSection = destinationLineIndex[0];
+    if (sourceSection === "bench" && destinationSection === "bench") {
+      // Reorder bench
+      const newBench = [...players];
+      newBench.splice(sourceIndex, 1);
+      newBench.splice(destinationIndex, 0, players[sourceIndex]);
+      console.log(newBench);
+      setPlayers(newBench);
     }
-    newLineup[destinationLineIndex][destinationIndex] = sourcePlayer;
-
-    console.log(newLineup);
-    setForwards(newLineup);
+    if (sourceSection === "bench" && destinationSection === "forwards") {
+      // Move bench to forwards
+      const destinationLine = destinationLineIndex[3];
+      const destinationPosition = destinationLineIndex[1];
+      const newForwards = { ...forwards };
+      console.log(getPlayerByCode(sourceIndex));
+      newForwards[destinationLine][destinationPosition] = getPlayerByCode(
+        sourceIndex
+      );
+      const newBench = [...players];
+      newBench.splice(sourceIndex, 1);
+      setForwards(newForwards);
+      setBench(newBench);
+      console.log(newForwards);
+    }
   };
-  console.log(forwards);
+  const getContent = (lineupSection, columnIndex, rowIndex) => {
+    if (lineupSection === "forwards") {
+      try {
+        console.log(forwards[rowIndex][columnIndex]);
+        if (forwards[rowIndex][columnIndex]) {
+          const player = forwards[rowIndex][columnIndex];
+          return (
+            <Draggable
+              key={parseInt(player.id, 10)}
+              draggableId={`draggable-${player.id}`}
+              index={parseInt(player.id, 10)}
+            >
+              {(provided, snapshot) => (
+                <PlayerCard
+                  className={classes.forwardCard}
+                  key={parseInt(player.id, 10)}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  player={player}
+                  ref={provided.innerRef}
+                />
+              )}
+            </Draggable>
+          );
+        }
+        return null;
+      } catch (e) {
+        if (e instanceof TypeError) {
+          return null;
+        }
+        throw e;
+      }
+    }
+  };
+  const makeDropZones = (lineupSection, columns, rowIndex) => {
+    let dropZones = [];
+    for (let i = 0; i < columns; i++) {
+      dropZones.push(
+        <Droppable
+          droppableId={`${lineupSection}-${i}-row-${rowIndex}`}
+          key={i}
+        >
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              className={clsx(
+                classes.dropZone,
+                snapshot.isDraggingOver && classes.dropZoneHighlight
+              )}
+            >
+              {getContent(lineupSection, i, rowIndex) &&
+              !snapshot.isDraggingOver ? (
+                getContent(lineupSection, i, rowIndex)
+              ) : (
+                <div>{getForwardPosition(i)}</div>
+              )}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      );
+    }
+    return dropZones;
+  };
+
   return (
     <div className={classes.root}>
       <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
@@ -98,73 +164,45 @@ function Roster() {
           <div>
             <h3>Forwards</h3>
             {/* Create 4 rows of 3 forward lines */}
-            {forwards.map((line, index) => (
-              <div key={index} className={classes.line}>
-                <Droppable
-                  droppableId={`forwards-${index}`}
-                  key={`forwards-${index}`}
-                >
-                  {(provided) => {
-                    return (
-                      <div
-                        className={classes.forwardArea}
-                        key={`forwards-${index}`}
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                      >
-                        {line.map((player, index) => (
-                          <Draggable
-                            key={player.id}
-                            draggableId={player.fullName}
-                            index={index}
-                            direction="horizontal"
-                          >
-                            {(provided) => (
-                              <PlayerCard
-                                style={{ ...provided.draggableProps.style }}
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                className={classes.forwardCard}
-                                key={player.id}
-                                player={player}
-                              />
-                            )}
-                          </Draggable>
-                        ))}
-                        {provided.placeholder}
-                      </div>
-                    );
-                  }}
-                </Droppable>
-              </div>
-            ))}
-          </div>
-          <div className={classes.defenceArea}>
-            {/* Defence */}
-            {defensemen.map((player) => (
-              <PlayerCard
-                key={player.id}
-                player={player}
-                className={classes.defenceCard}
-              />
-            ))}
-          </div>
-          <div className={classes.goalieArea}>
-            {/* Goalies */}
-            {goalies.map((player) => (
-              <PlayerCard
-                key={player.id}
-                player={player}
-                className={classes.goalieCard}
-              />
-            ))}
+            <div className={classes.forwardArea}>
+              {makeDropZones("forwards", 3, 1)}
+            </div>
+            <div className={classes.forwardArea}>
+              {makeDropZones("forwards", 3, 2)}
+            </div>
+            <div className={classes.forwardArea}>
+              {makeDropZones("forwards", 3, 3)}
+            </div>
+            <div className={classes.forwardArea}>
+              {makeDropZones("forwards", 3, 4)}
+            </div>
           </div>
         </div>
         <div className={classes.bench}>
-          {bench.map((player) => (
-            <PlayerCard key={player.id} player={player} />
-          ))}
+          <Droppable droppableId="bench">
+            {(provided, snapshot) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {players.map((player, index) => (
+                  <Draggable
+                    key={parseInt(player.id, 10)}
+                    draggableId={`draggable-${player.id}`}
+                    index={parseInt(player.id, 10)}
+                  >
+                    {(provided, snapshot) => (
+                      <PlayerCard
+                        key={parseInt(player.id, 10)}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        player={player}
+                        ref={provided.innerRef}
+                      />
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
         </div>
       </DragDropContext>
     </div>
